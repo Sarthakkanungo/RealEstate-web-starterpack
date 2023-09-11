4d46dc1f55961bb1129a33a8feceeb925f711589
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-09-11 18:52:49 +0530

    real state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Header } from "./Components/Header/Header";
+import { Hero } from "./Components/Hero/Hero";
+import "./App.css";
+import Companies from "./Components/Companies/Companies";
+import Residencies from "./Components/Residencies/Residencies";
+import Value from "./Components/Value/Value";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+    </div>
   );
 }
 
