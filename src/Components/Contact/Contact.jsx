3a6f69ce4433ea160444b76541989b1297572c05
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-09-15 12:42:04 +0530

    contact component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { MdCall } from "react-icons/md";
-import "../../index.css";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
+
 import "./Contact.css";
+
+
 const Contact = () => {
   return (
     <section className="c-wrapper">
       <div className="c-container">
-        <div className="c-left">
+        <div className=" flexColStart c-left">
           <span className="orangeText">Our Value</span>
           <span className="primaryText">How are you </span>
           <span className="secondaryText">
@@ -14,34 +18,77 @@ const Contact = () => {
             tempora harum. Alias perferendis laborum tempore reiciendis earum
             aliquam officiis soluta!
           </span>
-          <div className="row">
-            <div className="field1">
-              <div className="box1">
-                <div className="box-detail">
-                  <div className="icon">
-                    <MdCall size={25} className="icon-style" />
-                  </div>
-                  <div className="text">
-                    <span>Call</span>
-                    <span>278-178-876</span>
-                  </div>
-                </div>
-                <button aria-label="Call now">Call now</button>
-              </div>
 
-              <div className="box1">
-                <div className="icon">
-                  <MdCall size={25} />
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText" >Call</span>
+                    <span className="secondaryText">278-178-876</span>
+                  </div>
                 </div>
-                <div className="box-detail">
-                  <span>Call</span>
-                  <span>278-178-876</span>
+                <div className="flexCenter button">
+                  Call now
                 </div>
-                <button>call now</button>
+              </div>
+                  {/* second mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText" >Chat now</span>
+                    <span className="secondaryText">278-178-876</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">
+                  Call now
+                </div>
+              </div>
+            </div>
+
+            {/* second Row */}
+
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText" >Video call </span>
+                    <span className="secondaryText">278-178-876</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">
+                  Call now
+                </div>
+              </div>
+                  {/* fourth mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText" >message now</span>
+                    <span className="secondaryText">278-178-876</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">
+                  Call now
+                </div>
               </div>
             </div>
           </div>
         </div>
+
         <div className="c-right">
           <div className="image-container image-box">
             <img src="contact.jpg" alt="contact image" />
